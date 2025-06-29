984f364d0ed3e8bbed0a3f2a31a55d5da1a87d6d
Vasiliy Zheltikov <zholtikov1997@yandex.ru>
2025-06-29 14:38:44 +0300

    RemoveItemFromCartTest DONE

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -1,26 +1,41 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
 public class CartPage extends BasePage {
 
-    private By itemName = By.className("inventory_item_name");
-    private By itemPrice = By.className("inventory_item_price");
+    private final By ITEM = By.className("cart_item");
+    private final By ITEM_NAME = By.className("inventory_item_name");
+    private final By ITEM_PRICE = By.className("inventory_item_price");
+    private final By REMOVE_BUTTON = By.cssSelector("[class~=cart_button]");
 
     public CartPage(WebDriver driver) {
         super(driver);
     }
 
     public void open() {
-        driver.get(BASE_URL + "/cart.html");
+        driver.get(BASE_URL + "cart.html");
+    }
+
+    public boolean ItemIsDisplaying() {
+        try {
+            return driver.findElement(ITEM).isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
 
     public String getItemPrice() {
-        return driver.findElement(itemPrice).getText();
+        return driver.findElement(ITEM_PRICE).getText();
     }
 
     public String getItemName() {
-        return driver.findElement(itemName).getText();
+        return driver.findElement(ITEM_NAME).getText();
+    }
+
+    public void removeItemFromCart() {
+        driver.findElement(REMOVE_BUTTON).click();
     }
 }
