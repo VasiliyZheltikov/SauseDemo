7fcce5d64f959115005916c3e5ae68442b45c111
Vasiliy Zheltikov <zholtikov1997@yandex.ru>
2025-08-05 16:11:43 +0300

    Logged CheckoutYourInformationPage

--- src/test/java/pages/CheckoutYourInformationPage.java
+++ src/test/java/pages/CheckoutYourInformationPage.java
@@ -1,9 +1,11 @@
 package pages;
 
 import io.qameta.allure.Step;
+import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+@Log4j2
 public class CheckoutYourInformationPage extends BasePage {
 
     private final By FIRST_NAME_FIELD = By.id("first-name");
@@ -18,6 +20,7 @@ public class CheckoutYourInformationPage extends BasePage {
     @Step("Открытие страницы проверки заказа")
     @Override
     public CheckoutYourInformationPage open() { // Loadable Page, Chain of invocations
+        log.info("Opening CheckoutYourInformationPage");
         driver.get(BASE_URL + "checkout-step-one.html");
         return this;
     }
@@ -27,10 +30,19 @@ public class CheckoutYourInformationPage extends BasePage {
             "Last Name = {lastName}, " +
             "Postal Code = {postalCode}")
     public CheckoutOverviewPage login(String firstName, String lastName, String postalCode) { // Chain of invocations
+        log.info("Log in CheckoutOverviewPage");
+        log.info("Filling first name field...");
         driver.findElement(FIRST_NAME_FIELD).sendKeys(firstName);
+        log.info("First name has been successfully filled");
+        log.info("Filling last name field...");
         driver.findElement(LAST_NAME_FIELD).sendKeys(lastName);
+        log.info("Last name has been successfully filled");
+        log.info("Filling postal code field...");
         driver.findElement(POSTAL_CODE_FIELD).sendKeys(postalCode);
+        log.info("Postal code has been successfully filled");
+        log.info("Clicking at the Continue button...");
         driver.findElement(CONTINUE_BUTTON).click();
+        log.info("Continue button has been successfully clicked");
         return new CheckoutOverviewPage(driver);
     }
 }
