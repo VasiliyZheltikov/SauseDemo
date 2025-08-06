0ef65fdd56e24569a904076d04f33fb22f22d45b
Василий Жёлтиков <108517372+VasiliyZheltikov@users.noreply.github.com>
2025-08-06 15:22:56 +0300

    Logging (#6)
    * Realized Chain Of Invocation pattern and Loadable Page pattern (added comments)
    
    * Added logger
    
    * Logged CartPage
    
    * Logged CheckoutCompletePage
    
    * Logged CheckoutOverviewPage
    
    * Logged CheckoutYourInformationPage
    
    * Logged LoginPage
    
    * Logged ProductsPage
    
    * Logged Retry and TestListener
    
    * Added params to logs

--- src/test/java/pages/CheckoutOverviewPage.java
+++ src/test/java/pages/CheckoutOverviewPage.java
@@ -1,9 +1,11 @@
 package pages;
 
 import io.qameta.allure.Step;
+import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+@Log4j2
 public class CheckoutOverviewPage extends BasePage {
 
     private final By ITEM_NAME = By.className("inventory_item_name");
@@ -17,22 +19,26 @@ public class CheckoutOverviewPage extends BasePage {
     @Step("Открытие окна подтверждения заказа")
     @Override
     public CheckoutOverviewPage open() { // Loadable Page, Chain of invocations
+        log.info("Opening CheckoutOverviewPage");
         driver.get(BASE_URL + "checkout-step-two.html");
         return this;
     }
 
     @Step("Нахождение наименования товара на странице подтверждения заказа")
     public String getItemName() {
+        log.info("Finding item's name at the CheckoutOverviewPage");
         return driver.findElement(ITEM_NAME).getText();
     }
 
     @Step("Нахождение цены товара на странице подтверждения заказа")
     public String getItemPrice() {
+        log.info("Finding item's price at the CheckoutOverviewPage");
         return driver.findElement(ITEM_PRICE).getText();
     }
 
     @Step("Нажатие кнопки оформления заказа")
     public CheckoutCompletePage finish() { // Chain of invocations
+        log.info("Clicking at the Finish button at the CheckoutOverviewPage");
         driver.findElement(FINISH_BUTTON).click();
         return new CheckoutCompletePage(driver);
     }
--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,9 +1,11 @@
 package pages;
 
 import io.qameta.allure.Step;
+import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+@Log4j2
 public class LoginPage extends BasePage {
 
     private final By LOGIN_FIELD = By.id("user-name");
@@ -18,21 +20,30 @@ public class LoginPage extends BasePage {
     @Step("Открытие сайта SauseDemo")
     @Override
     public LoginPage open() { // Loadable Page, Chain of invocations
+        log.info("Opening LoginPage");
         driver.get(BASE_URL);
         return this;
     }
 
     @Step("Вход в систему с именем пользователя {user} и паролем {password}")
     public ProductsPage login(String user, String password) { // Chain of invocations
+        log.info("Log in LoginPage");
+        log.info("Filling Login field: {}", user);
         driver.findElement(LOGIN_FIELD).sendKeys(user); // заполнение Username
+        log.info("Login field has been successfully filled");
+        log.info("Filling Password field {}", password);
         driver.findElement(PASSWORD_FIELD).sendKeys(password); // заполнение Password
+        log.info("Password field has been successfully filled");
+        log.info("Clicking on the Login button...");
 //        clickJS(driver.findElement(LOGIN_BUTTON));
         driver.findElement(LOGIN_BUTTON).click();
+        log.info("Login button has been successfully clicked");
         return new ProductsPage(driver);
     }
 
     @Step("Получение текста ошибки авторизации в системе")
     public String getErrorMessage() {
+        log.info("Getting error message (wrong auth data)");
         return driver.findElement(ERROR_MESSAGE).getText();
     }
 }
