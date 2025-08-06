5f710a6f89ec51e52581737d29c6f434e06e9dd9
Vasiliy Zheltikov <zholtikov1997@yandex.ru>
2025-08-06 16:43:58 +0300

    Added secrets

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -26,8 +26,8 @@ public class BaseTest {
     CheckoutOverviewPage checkoutOverviewPage;
     CheckoutCompletePage checkoutCompletePage;
 
-    String user = System.getProperty("user");
-    String password = System.getProperty("password");
+    String user = System.getProperty("user", PropertyReader.getProperty("user"));
+    String password = System.getProperty("password", PropertyReader.getProperty("password"));
 
     @Parameters({"browser"})
     @BeforeMethod(alwaysRun = true, description = "Настройка драйвера")
